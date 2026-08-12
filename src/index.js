d3f3730b12b52dcb09ed55b78519803157395a3b
arthika-m <arthika@student.tce.edu>
2026-08-12 15:18:09 +0530

    Add Auth0Provider for authentication
    Integrate Auth0 for authentication in the app.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 
 import App from "./App";
-
 import { AuthProvider } from "./AuthContext";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
 root.render(
+  <React.StrictMode>
 
-<React.StrictMode>
+    <AuthProvider>
 
-<AuthProvider>
+      <Auth0Provider
+        domain="dev-wesy33w1g8gd7cy6.us.auth0.com"
+        clientId="l1PMr0YjuXUcskEr16APUw6tN804wYId"
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+      >
 
-<App />
+        <App />
 
-</AuthProvider>
+      </Auth0Provider>
 
-</React.StrictMode>
+    </AuthProvider>
 
+  </React.StrictMode>
 );
